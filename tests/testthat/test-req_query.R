373118b4a5e47a86825046936eee5b2cf6b19dca
Jon Harmon <jonthegeek@gmail.com>
2024-04-02 14:37:51 -0500

    Drop empty query args. (#21)

--- tests/testthat/test-req_query.R
+++ tests/testthat/test-req_query.R
@@ -27,3 +27,18 @@ test_that("req_prepare() smushes & concatenates multi-value query parameters", {
     "https://example.com/?foo=bar&baz=qux%2Cquux"
   )
 })
+
+test_that("req_prepare() removes empty query parameters", {
+  test_result <- req_prepare(
+    base_url = "https://example.com",
+    query = list(
+      foo = NULL,
+      bar = "baz"
+    ),
+    user_agent = NULL
+  )
+  expect_identical(
+    test_result$url,
+    "https://example.com/?bar=baz"
+  )
+})
